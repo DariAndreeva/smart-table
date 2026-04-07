37bd866dfc8200f3b973e4c919c09c8490ece6d0
Дарья <di213andreeva@yandex.com>
2026-04-07 20:02:04 +0300

    Выполнила 1 задания

--- src/main.js
+++ src/main.js
@@ -14,7 +14,8 @@ import { initSearching } from "./components/searching.js";
 // @todo: подключение
 
 // Исходные данные используемые в render()
-const { data, ...indexes } = initData(sourceData);
+//const { data } = initData(sourceData);
+const API = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
@@ -37,18 +38,19 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
+async function render(action) {
   let state = collectState(); // состояние полей из таблицы
-  let result = [...data]; // копируем для последующего изменения
+  let query = {}; // копируем для последующего изменения
   // @todo: использование
 
-  //result = applySearching(result, state, action);
-  result = applySearching(result, state, action);
-  result = applySorting(result, state, action);
-  result = applyPagination(result, state, action);
-  result = applyFiltering(result, state, action);
+  // result = applySearching(result, state, action);
+  // result = applySorting(result, state, action);
+  // result = applyPagination(result, state, action);
+  // result = applyFiltering(result, state, action);
 
-  sampleTable.render(result);
+  const { total, items } = await API.getRecords(query);
+
+  sampleTable.render(items);
 }
 
 const sampleTable = initTable(
@@ -79,14 +81,17 @@ const applySorting = initSorting([
   sampleTable.header.elements.sortByTotal,
 ]);
 
-const applyFiltering = initFiltering(sampleTable.filter.elements, {
-  // передаём элементы фильтра
-  searchBySeller: indexes.sellers, // для элемента с именем searchBySeller устанавливаем массив продавцов
-});
+// const applyFiltering = initFiltering(sampleTable.filter.elements, {
+//   // передаём элементы фильтра
+//   searchBySeller: indexes.sellers, // для элемента с именем searchBySeller устанавливаем массив продавцов
+// });
 
 const applySearching = initSearching("search");
 
 const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+  const indexes = await API.getIndexes();
+}
+init().then(render);
