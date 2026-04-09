cca1603a09855878d9039f70fafe9c511cf8d298
Дарья <di213andreeva@yandex.com>
2026-04-09 19:23:48 +0300

    Закончила проектую работу

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,36 +1,52 @@
-import { createComparison, defaultRules } from "../lib/compare.js";
+// import { createComparison, defaultRules } from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
-const compare = createComparison(defaultRules);
+// const compare = createComparison(defaultRules);
 
-export function initFiltering(elements, indexes) {
-  // @todo: #4.1 — заполнить выпадающие списки опциями
-  Object.keys(indexes).forEach((elementName) => {
-    elements[elementName].append(
-      ...Object.values(indexes[elementName]).map((name) => {
-        const option = document.createElement("option");
-        option.value = name;
-        option.textContent = name;
-        return option;
-      }),
-    );
-  });
+export function initFiltering(elements) {
+  const updateIndexes = (elements, indexes) => {
+    // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes).forEach((elementName) => {
+      elements[elementName].append(
+        ...Object.values(indexes[elementName]).map((name) => {
+          const el = document.createElement("option");
+          el.textContent = name;
+          el.value = name;
+          return el;
+        }),
+      );
+    });
+  };
 
-  return (data, state, action) => {
+  const applyFiltering = (query, state, action) => {
     // @todo: #4.2 — обработать очистку поля
     if (action && action.name === "clear") {
       const parent = action.parentElement;
-      const inpunt = parent.querySelector("input");
-      if (inpunt) {
-        inpunt.value = "";
+      const input = parent.querySelector("input");
+      if (input) {
+        input.value = "";
         const fieldName = action.dataset.field;
         if (fieldName) {
           state[fieldName] = "";
         }
       }
     }
+    const filter = {};
+    Object.keys(elements).forEach((key) => {
+      if (elements[key]) {
+        if (
+          ["INPUT", "SELECT"].includes(elements[key].tagName) &&
+          elements[key].value
+        ) {
+          filter[`filter[${elements[key].name}]`] = elements[key].value;
+        }
+      }
+    });
+    return Object.keys(filter).length ? Object.assign({}, query.filter) : query;
+  };
 
-    // @todo: #4.5 — отфильтровать данные используя компаратор
-    return data.filter((row) => compare(row, state));
+  return {
+    updateIndexes,
+    applyFiltering,
   };
 }
--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import { initSearching } from "./components/searching.js";
 
 // Исходные данные используемые в render()
 //const { data } = initData(sourceData);
-const API = initData(sourceData);
+const api = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
@@ -43,13 +43,14 @@ async function render(action) {
   let query = {}; // копируем для последующего изменения
   // @todo: использование
 
-  // result = applySearching(result, state, action);
-  // result = applySorting(result, state, action);
-  // result = applyPagination(result, state, action);
-  // result = applyFiltering(result, state, action);
+  query = applySearching(query, state, action);
+  query = applyFiltering(query, state, action);
+  query = applySorting(query, state, action);
+  query = applyPagination(query, state, action);
 
-  const { total, items } = await API.getRecords(query);
+  const { total, items } = await api.getRecords(query);
 
+  updatePagination(total, query); // перерисовываем пагинатор
   sampleTable.render(items);
 }
 
@@ -65,7 +66,7 @@ const sampleTable = initTable(
 
 // @todo: инициализация
 
-const applyPagination = initPagination(
+const { applyPagination, updatePagination } = initPagination(
   sampleTable.pagination.elements,
   (el, page, isCurrent) => {
     const input = el.querySelector("input");
@@ -76,15 +77,16 @@ const applyPagination = initPagination(
     return el;
   },
 );
+
 const applySorting = initSorting([
   sampleTable.header.elements.sortByDate,
   sampleTable.header.elements.sortByTotal,
 ]);
 
-// const applyFiltering = initFiltering(sampleTable.filter.elements, {
-//   // передаём элементы фильтра
-//   searchBySeller: indexes.sellers, // для элемента с именем searchBySeller устанавливаем массив продавцов
-// });
+const { applyFiltering, updateIndexes } = initFiltering(
+  sampleTable.filter.elements,
+);
+// передаём элементы фильтра
 
 const applySearching = initSearching("search");
 
@@ -92,6 +94,10 @@ const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
 
 async function init() {
-  const indexes = await API.getIndexes();
+  const indexes = await api.getIndexes();
+
+  updateIndexes(sampleTable.filter.elements, {
+    searchBySeller: indexes.sellers,
+  });
 }
 init().then(render);
